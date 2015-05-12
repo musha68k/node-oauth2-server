70736b680e180fc0c46a5417d1949ad85edfd864
cesine <cesine@yahoo.com>
2015-05-11 21:56:00 -0400

    converted test data creation into couchdb

--- examples/couchdb/README.md
+++ examples/couchdb/README.md
@@ -1,6 +1,10 @@
-# Redis Example
+# CouchDB Example
 
-A simple example with support for `password` and `refresh_token` grants using [Redis](http://redis.io/). You'll need [node-redis](https://github.com/mranney/node_redis) installed.
+A simple example with support for `password` and `refresh_token` grants using [CouchDB](http://couchdb.apache.org/). You'll need [nano](https://www.npmjs.com/package/nano) installed.
+
+```bash
+$ npm install nano --save
+```
 
 ## Usage
 
@@ -14,6 +18,6 @@ app.oauth = oauthserver({
 
 ## Data model
 
-The example makes use of a simple data model where clients, tokens, refresh tokens and users are stored as [hashes](http://redis.io/topics/data-types#hashes). The allowed grants for each client are stored in a [set](http://redis.io/topics/data-types#sets) `clients:{id}:grant_types`. This allows grants to be added or removed dynamically. To simplify the user lookup users are identified by their username and not by a separate ID. Passwords are stored in the clear for simplicity, but in practice these should be hashed using a library like [bcrypt](https://github.com/ncb000gt/node.bcrypt.js).
+Passwords are stored in the clear for simplicity, but in practice these should be hashed using a library like [bcrypt](https://github.com/ncb000gt/node.bcrypt.js).
 
-To inject some test data you can run the `testData.js` script in this directory. This will create a client with the ID `client` and secret `secret` and create a single user with the username `username` and password `password`.
+To create the databases and inject some test data you can run the `testData.js` script in this directory. This will create a client with the ID `afancyagregatorapp` and secret `donttelltheagregatorssecret` and create a single user with the username `bob` and password `buffalo`.
--- examples/couchdb/testData.js
+++ examples/couchdb/testData.js
@@ -1,27 +1,103 @@
 #! /usr/bin/env node
 
-var db = require('redis').createClient();
+var couchdbConfig = require('./couchdb.json'),
+  util = require('util');
 
-db.multi()
-  .hmset('users:username', {
-    id: 'username',
-    username: 'username',
-    password: 'password'
-  })
-  .hmset('clients:client', {
-    clientId: 'client',
-    clientSecret: 'secret'
-  })
-  .sadd('clients:client:grant_types', [
-    'password',
-    'refresh_token'
-  ])
-  .exec(function (errs) {
-    if (errs) {
-      console.error(errs[0].message);
-      return process.exit(1);
+/**
+ * Creates some sample docs
+ * @return null
+ */
+var createDocs = function() {
+  var sampleUser = {
+    _id: 'bob',
+    username: 'bob',
+    password: 'buffalo'
+  };
+
+  var sampleClientApp = {
+    _id: 'afancyagregatorapp',
+    clientId: 'afancyagregatorapp',
+    clientSecret: 'donttelltheagregatorssecret',
+    grant_types: [
+      'password',
+      'refresh_token'
+    ]
+  };
+
+  console.log('Creating some sample docs..')
+
+  var userdb = require('nano')(couchdbConfig.users_url);
+  userdb.insert(sampleUser, sampleUser.username, function(error, resultuser) {
+    if (error) {
+      if (error.statusCode === 409) {
+        console.log('  Your user ' + sampleUser._id + ' already exists, no need to create it.');
+      } else {
+        console.log('  Created a sample user: ' + util.inspect(error));
+      }
+
+    } else {
+      if (!resultuser.ok) {
+        console.log(new Date() + '  No error creating a user, but response was not okay: ' + util.inspect(resultuser));
+      } else {
+        console.log(new Date() + '  No error saving a user: ' + util.inspect(resultuser));
+      }
     }
-
-    console.log('Client and user added successfully');
-    process.exit();
   });
+
+  var clientdb = require('nano')(couchdbConfig.oauth_clients_url);
+  clientdb.insert(sampleClientApp, sampleClientApp.username, function(error, resultuser) {
+    if (error) {
+      if (error.statusCode === 409) {
+        console.log('  Your client app ' + sampleClientApp._id + ' already exists, no need to create it.');
+      } else {
+        console.log('  Created a sample client: ' + util.inspect(error));
+      }
+
+    } else {
+      if (!resultuser.ok) {
+        console.log(new Date() + '  No error creating a client, but response was not okay: ' + util.inspect(resultuser));
+      } else {
+        console.log(new Date() + '  No error saving a client: ' + util.inspect(resultuser));
+      }
+    }
+    // process.exit();
+  });
+};
+
+/**
+ * Creates some sample databases (recursively so that each is created in sequence when the previous has been created)
+ * @param  {Array} of urls to be created, could be on any couchdb server
+ * @return null
+ */
+var createDbAndLoop = function(fullUrls) {
+  if (!fullUrls || fullUrls.length === 0) {
+    createDocs();
+    return;
+  }
+  var fullUrl = fullUrls.shift();
+  var dbname = fullUrl.substring(fullUrl.lastIndexOf('/')).replace('/', '');
+  var couchdbUrlForThisDatabase = fullUrl.replace('/' + dbname, '');
+
+  console.log('Creating ' + dbname + ' on ' + couchdbUrlForThisDatabase);
+  var server = require('nano')(couchdbUrlForThisDatabase);
+  try {
+    server.db.create(dbname, function(error, response) {
+      if (error) {
+        if (error.statusCode === 412) {
+          console.log('  Your db ' + fullUrl + ' already exists, no need to create it.');
+        } else {
+          console.log('  The server was unable to create this database ', util.inspect(error));
+        }
+      } else {
+        console.log('  Created ' + fullUrl, response);
+      }
+      createDbAndLoop(fullUrls);
+    });
+  } catch (e) {
+    console.log(e);
+    createDbAndLoop(fullUrls);
+  }
+};
+
+// Create databases and docs.
+createDbAndLoop([couchdbConfig.users_url, couchdbConfig.oauth_access_tokens_url, couchdbConfig.oauth_clients_url, couchdbConfig.oauth_refresh_tokens_url]);
